ecd67f2229c9e39fc1ec8a8d366e241bc0a4c9a0
autolab <autolab@Gort.(none)>
2012-05-16 14:58:26 -0700

    Add `mkdir lib` to build_sdk.sh

--- build_sdk.sh
+++ build_sdk.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cd ARDroneLib/Soft/Build && make clean && make && cp -f targets_versions/*/*.a ../../../lib/
+cd mkdir lib && ARDroneLib/Soft/Build && make clean && make && cp -f targets_versions/*/*.a ../../../lib/
 cd ../../../
