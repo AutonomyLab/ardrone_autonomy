10a829add140f4db842e7003a42bcc93d2ee6e9f
autolab <autolab@Gort.(none)>
2012-05-16 15:01:09 -0700

    Added remove lib directory and re-create it again to build_sdk to do a clean clean build

--- build_sdk.sh
+++ build_sdk.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-mkdir lib && cd ARDroneLib/Soft/Build && make clean && make && cp -f targets_versions/*/*.a ../../../lib/
+rm -rf ./lib && mkdir lib && cd ARDroneLib/Soft/Build && make clean && make && cp -f targets_versions/*/*.a ../../../lib/
 cd ../../../
